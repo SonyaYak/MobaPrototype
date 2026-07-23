a2b821cc39e43b7e3fc35f2b9c10aeb938d81e94
SonyaYak <sofya.www@inbox.ru>
2026-07-23 18:52:11 +0300

    feat: implement MoveableUnit abstract class for navigation
    - Created abstract MoveableUnit class with [RequireComponent(typeof(NavMeshAgent))]
    - Added virtual SetDestination method to handle NavMeshAgent pathfinding

---
+++ Assets/Scripts/Core/MoveableUnit.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+// Note: All moving units will inherit from this class instead of standard Unit
+public abstract class MoveableUnit : Unit
+{
+    protected NavMeshAgent _navMeshAgent;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    protected void SetStopDistance(float distance) => _navMeshAgent.stoppingDistance = distance;
+
+    // NavMeshAgent.SetDestination takes exact 3D coordinates on the baked mesh.
+    protected virtual void SetDestination(Vector3 direction) => _navMeshAgent.SetDestination(direction);
+}
